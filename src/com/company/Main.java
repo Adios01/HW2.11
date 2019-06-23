abe951b50f7d289c5a06ea089dca51895ba3349f
adi <kubatbekov888@gmail.com>
2019-06-23 22:29:45 +0600

    add random

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("Случайное число " + ranAge);
 
         Random t = new Random();
-        int ranT memp = t.nextInt(30) -20;
+        int ranTemp = t.nextInt(30) -20;
         System.out.println("Случайное число " + ranTemp);
 
         String name = "Адилет";
